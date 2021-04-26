7614583c1b5af67d56172ee0afd5c99ea65e9ac1
Юлия <yuliya.karanevskaya@gmail.com>
2021-04-26 16:16:49 +0300

    Initial Commit

---
+++ Plantus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plantus
+//
+//  Created by Юлия Караневская on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plantus/PlantusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantusApp.swift
+//  Plantus
+//
+//  Created by Юлия Караневская on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PlantusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
